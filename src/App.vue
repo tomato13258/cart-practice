3b6ece7403cf4532a6f308dac0c86c5ed9042919
tomato13258 <160811525+tomato13258@users.noreply.github.com>
2024-03-05 09:09:40 +0800

    增加ㄌ購物車頁面

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,34 @@
-<script setup>
+<script>
+import switchBtns from '@/components/switch-ntns.vue'
+
 // import { RouterLink, RouterView } from 'vue-router'
 // import HelloWorld from './components/HelloWorld.vue'
-import switchNtns from '@/components/switch-ntns.vue'
+
+// 可以試做返回紐在主頁面?
+export default{
+  data(){
+    return{
+      showValue:true,
+
+    }
+  },
+  methods:{
+    hiddenBtns(){
+      this.showValue=false;
+    },
+    returnShow(e){
+      
+      this.showValue=e;
+
+    },
+    
+
+  },
+  components:{
+    switchBtns,
+
+  },
+};
 </script>
 
 <template>
@@ -9,13 +36,13 @@ import switchNtns from '@/components/switch-ntns.vue'
     <nav class="w-full h-[50px] bg-slate-700  flex items-center justify-between py-[30px] px-[80px] mb-[50px]">
       <div class="text-gray-100">LOGO-SHOPPING</div>
       <div class="nav-right-box">
-        <router-link to="/cartPage" class=""><img src="./components/icons/cart-fill.svg" alt=""></router-link>
+        <router-link to="/cartPage" class="" @click="hiddenBtns()"><img src="./components/icons/cart-fill.svg" alt=""></router-link>
         <div class="cart-circle w-[18px] h-[18px] bg-red-500">
           <span class=" text-gray-100 text-[14px]">0</span>
         </div>
       </div>
     </nav>
-    <switchNtns/>
+   <switchBtns v-show="showValue" @returnClick="returnShow" />
     <RouterView />
     <footer class="w-full h-[100px] bg-slate-700"></footer>
   </div>
--- src/views/cartPage.vue
+++ src/views/cartPage.vue
@@ -1,41 +1,60 @@
-<script setup>
-// import switchNtns from '@/components/switch-ntns.vue'
+<script>
+import returnBtn from '@/components/returnBtn.vue';
+import cartItemCard from '@/components/cartItemCard.vue'
 
-// import TheWelcome from '../components/TheWelcome.vue'
+
+export default {
+  components: {
+    returnBtn,
+    cartItemCard,
+
+  },
+  data() {
+    return {
+      returnValue: true,
+
+    }
+  },
+  emits: ['returnClick'],
+  methods: {
+    // 回傳值給父層
+    returnClick() {
+      this.$emit('returnClick', this.returnValue);
+      console.log(this.returnValue);
+    },
+  },
+};
 </script>
 
+
 <template>
-    <main class="w-full bg-neutral-200 px-[8%] py-[5%] ">
-    
-    <div class="items-box  flex justify-around gap-[20px]  gap-y-[50px] flex-wrap ">
+  <main class="w-full bg-neutral-200 px-[8%] py-[5%] ">
+
+    <div class="items-box  flex justify-around flex-wrap mb-[80px]">
       <!-- 這裏面放商品卡 -->
-      <div class="w-[80%] h-[250px] bg-slate-500  rounded-[5px] flex ">
-        <img src="https://fakeimg.pl/250x250/000000,100/f7c995,200" class="">
-        <div class="item-text-box  px-[10px]">
-          <div class=" text-[28px]  font-bold text-neutral-200 mb-[10px]">我是商品</div>
-          <div class=" text-[14px] text-neutral-300 mb-[50px]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been  </div>
-          <div class="text-[#f7c995] mb-[20px] font-bold">＄400 NT</div>
-          <!--按鈕組 -->
-          <div class="">
-            <div class="flex justify-start mb-[5px]">
-              <button class="itemBtn w-[30px] h-[30px] bg-slate-800 flex items-center justify-center text-white">-</button>
-              <div class="bg-gray-100 w-full  h-[30px] text-[20px] text-center">1</div>
-              <button class="itemBtn w-[30px] h-[30px] bg-slate-800 flex items-center justify-center text-white">+</button>
-            </div>
-            <div class="add-cart w-[100%] h-[30px] bg-slate-800 flex items-center justify-center text-white text-[16px] rounded-[5px]">加入購物車</div>
-          </div>
+      <cartItemCard />
+      <cartItemCard />
+      <cartItemCard />
+      <div class="flex justify-around bg-slate-600 w-[80%] py-[30px] text-gray-200">
+        <div class="">
+          <span>商品數量:</span>
+          <span class="">2</span>
+        </div>
+        <div class="">
+          <span>商品總金額:</span>
+          <span class="">5000</span>
+          <span class="">元</span>
         </div>
       </div>
+    </div>
+
+
+
+    <div class="flex justify-around">
+      <returnBtn content="返回上一頁" bgColor="bg-slate-600" @click="returnClick" />
+      <returnBtn content="下一步" bgColor="bg-[#c98333]" />
     </div>
   </main>
 </template>
 
-<style scoped>
-.itemBtn,.add-cart{
-  transition:  .3s;
-}
-.itemBtn:hover,.add-cart:hover{
-  background-color: #c98333;
- 
-}
-</style>
+<style scoped></style>
